3f52ad1f5c497e8a4a9c499cc1823bbf0d1285bd
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-06-06 00:32:24 -0400

    Added Server.js, started making the routes

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+    // 
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
